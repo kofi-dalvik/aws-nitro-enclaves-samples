f0abde184857d7b8c999c8a787561b20923316c1
David L. Adei <kofidahmed@gmail.com>
2024-04-08 23:23:56 +0000

    Temp: implementing recevall for client

--- vsock_sample/py/client.py
+++ vsock_sample/py/client.py
@@ -1,4 +1,4 @@
-import socket 
+import socket, hashlib
 
 PORT = 5005
 HEADER = 64
@@ -12,19 +12,30 @@ def connnect():
     client.connect(ADDR)
     return client
 
-def send(conn: socket.socket, msg: str):
+def recvall(conn: socket.socket) -> str:
+    res: str = ''
+    while True:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            break
+
+def send(conn: socket.socket, msg: str) -> str:
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b" " * (HEADER - len(send_length))
     
     conn.send(send_length)
-    conn.send(message)
-    # client.send(DISCONNECT_MESSAGE.encode(FORMAT))
-    print(conn.recv(2048).decode(FORMAT))
+    conn.sendall(message)
+    return conn.recv(2048).decode(FORMAT)
+    
     
 
 if __name__ == "__main__":
     connection: socket.socket = connnect()
-    send(connection, 'Hello World!')
+    msg: str = 'Hello World!' * (1024 * 1024)
+    res: str = send(connection, msg=msg)
+    assert res == hashlib.sha256(msg.encode(FORMAT)).hexdigest()
+    print(res)
     send(connection, DISCONNECT_MESSAGE)
--- vsock_sample/py/server.py
+++ vsock_sample/py/server.py
@@ -1,8 +1,9 @@
-import socket, threading
+import socket, threading, hashlib
 
 PORT = 5005
 HEADER = 64
 FORMAT = "utf-8"
+BUFFER_SIZE = 2048
 DISCONNECT_MESSAGE = "!disconnect!"
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
@@ -10,19 +11,37 @@ ADDR = (SERVER, PORT)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+def recvall(conn: socket.socket, msg_length: int):
+    msg = ''
+    while len(msg) < msg_length:
+        num_bytes = min(BUFFER_SIZE, msg_length - len(msg))
+        m = conn.recv(num_bytes).decode(FORMAT)
+        # print('Size: ', num_bytes, 'Message: ', m)
+        msg += m
+    return msg
+
 def handle_client(conn: socket.socket, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
-    connected = True
-    while connected:
+    while True:
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
+            print('Message length: ', msg_length)
+            msg = recvall(conn, msg_length)
+            # print('Message received: ', msg)
+            
             if msg == DISCONNECT_MESSAGE:
-                connected = False
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
-
+                break
+                
+            # print(f"[{addr}] {msg}")
+            print('Processing request...')
+            print('Message: ', msg)
+            res: str = hashlib.sha256(msg.encode(FORMAT)).hexdigest()
+            conn.send(msg.encode(FORMAT))
+            
+    conn.close()
+    print(f"[CONNECTION CLOSED] {addr} disconnected.")
+    
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}:{PORT}")
